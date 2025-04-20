5eb486011ddc7cce80b957d47736bc2d7295ea05
Dev Raghuwanshi <devr4569@gmail.com>
2025-04-20 13:30:31 +0530

    84A-ToyArmy

---
+++ A_Toy_Army.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+ 
+void solve() {
+    long long n;
+    cin>>n;
+    long long result=floor(3*(n/2.0));
+    cout<<result<<endl;
+}
+
+int main() {
+    fastIO(); 
+ 
+    int t = 1;
+    // cin >> t;
+ 
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
